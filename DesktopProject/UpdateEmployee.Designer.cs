076dd4ae845092e6e9162471584db82f0ebfd423
Ragini Jangid <raginijangid55@gmail.com>
2024-09-25 22:37:49 +0530

    Remove image from home

--- DesktopProject/UpdateEmployee.Designer.cs
+++ DesktopProject/UpdateEmployee.Designer.cs
@@ -51,9 +51,10 @@ namespace DesktopProject
             this.UpdateEmpButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.UpdateEmpButton.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.UpdateEmpButton.ForeColor = System.Drawing.SystemColors.Info;
-            this.UpdateEmpButton.Location = new System.Drawing.Point(626, 482);
+            this.UpdateEmpButton.Location = new System.Drawing.Point(556, 386);
+            this.UpdateEmpButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UpdateEmpButton.Name = "UpdateEmpButton";
-            this.UpdateEmpButton.Size = new System.Drawing.Size(165, 50);
+            this.UpdateEmpButton.Size = new System.Drawing.Size(147, 40);
             this.UpdateEmpButton.TabIndex = 36;
             this.UpdateEmpButton.Text = "UPDATE";
             this.UpdateEmpButton.UseMnemonic = false;
@@ -62,97 +63,103 @@ namespace DesktopProject
             // 
             // addEmpCityTxtBox
             // 
-            this.addEmpCityTxtBox.Location = new System.Drawing.Point(556, 392);
+            this.addEmpCityTxtBox.Location = new System.Drawing.Point(494, 314);
+            this.addEmpCityTxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addEmpCityTxtBox.Name = "addEmpCityTxtBox";
-            this.addEmpCityTxtBox.Size = new System.Drawing.Size(349, 26);
+            this.addEmpCityTxtBox.Size = new System.Drawing.Size(311, 22);
             this.addEmpCityTxtBox.TabIndex = 35;
             // 
             // empCity
             // 
             this.empCity.AutoSize = true;
             this.empCity.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.empCity.Location = new System.Drawing.Point(429, 394);
+            this.empCity.Location = new System.Drawing.Point(381, 315);
             this.empCity.Name = "empCity";
-            this.empCity.Size = new System.Drawing.Size(105, 22);
+            this.empCity.Size = new System.Drawing.Size(86, 18);
             this.empCity.TabIndex = 34;
             this.empCity.Text = "Emp City ";
             // 
             // addEmpPhoneTxtBox
             // 
-            this.addEmpPhoneTxtBox.Location = new System.Drawing.Point(556, 340);
+            this.addEmpPhoneTxtBox.Location = new System.Drawing.Point(494, 272);
+            this.addEmpPhoneTxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addEmpPhoneTxtBox.Name = "addEmpPhoneTxtBox";
-            this.addEmpPhoneTxtBox.Size = new System.Drawing.Size(349, 26);
+            this.addEmpPhoneTxtBox.Size = new System.Drawing.Size(311, 22);
             this.addEmpPhoneTxtBox.TabIndex = 33;
             // 
             // empPhone
             // 
             this.empPhone.AutoSize = true;
             this.empPhone.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.empPhone.Location = new System.Drawing.Point(429, 342);
+            this.empPhone.Location = new System.Drawing.Point(381, 274);
             this.empPhone.Name = "empPhone";
-            this.empPhone.Size = new System.Drawing.Size(123, 22);
+            this.empPhone.Size = new System.Drawing.Size(100, 18);
             this.empPhone.TabIndex = 32;
             this.empPhone.Text = "Emp Phone";
             // 
             // addEmpEmailTxtBox
             // 
-            this.addEmpEmailTxtBox.Location = new System.Drawing.Point(556, 287);
+            this.addEmpEmailTxtBox.Location = new System.Drawing.Point(494, 230);
+            this.addEmpEmailTxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addEmpEmailTxtBox.Name = "addEmpEmailTxtBox";
-            this.addEmpEmailTxtBox.Size = new System.Drawing.Size(349, 26);
+            this.addEmpEmailTxtBox.Size = new System.Drawing.Size(311, 22);
             this.addEmpEmailTxtBox.TabIndex = 31;
             // 
             // empEmail
             // 
             this.empEmail.AutoSize = true;
             this.empEmail.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.empEmail.Location = new System.Drawing.Point(429, 289);
+            this.empEmail.Location = new System.Drawing.Point(381, 231);
             this.empEmail.Name = "empEmail";
-            this.empEmail.Size = new System.Drawing.Size(121, 22);
+            this.empEmail.Size = new System.Drawing.Size(98, 18);
             this.empEmail.TabIndex = 30;
             this.empEmail.Text = "Emp Email ";
             // 
             // addEmpNameTxtBox
             // 
-            this.addEmpNameTxtBox.Location = new System.Drawing.Point(556, 231);
+            this.addEmpNameTxtBox.Location = new System.Drawing.Point(494, 185);
+            this.addEmpNameTxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addEmpNameTxtBox.Name = "addEmpNameTxtBox";
-            this.addEmpNameTxtBox.Size = new System.Drawing.Size(349, 26);
+            this.addEmpNameTxtBox.Size = new System.Drawing.Size(311, 22);
             this.addEmpNameTxtBox.TabIndex = 29;
             // 
             // empName
             // 
             this.empName.AutoSize = true;
             this.empName.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.empName.Location = new System.Drawing.Point(429, 233);
+            this.empName.Location = new System.Drawing.Point(381, 186);
             this.empName.Name = "empName";
-            this.empName.Size = new System.Drawing.Size(118, 22);
+            this.empName.Size = new System.Drawing.Size(97, 18);
             this.empName.TabIndex = 28;
             this.empName.Text = "Emp Name";
             // 
             // addEmpIdTxtBox
             // 
-            this.addEmpIdTxtBox.Location = new System.Drawing.Point(556, 181);
+            this.addEmpIdTxtBox.Location = new System.Drawing.Point(494, 145);
+            this.addEmpIdTxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addEmpIdTxtBox.Name = "addEmpIdTxtBox";
-            this.addEmpIdTxtBox.Size = new System.Drawing.Size(349, 26);
+            this.addEmpIdTxtBox.Size = new System.Drawing.Size(311, 22);
             this.addEmpIdTxtBox.TabIndex = 27;
             // 
             // EmpId
             // 
             this.EmpId.AutoSize = true;
             this.EmpId.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.EmpId.Location = new System.Drawing.Point(429, 183);
+            this.EmpId.Location = new System.Drawing.Point(381, 146);
             this.EmpId.Name = "EmpId";
-            this.EmpId.Size = new System.Drawing.Size(91, 22);
+            this.EmpId.Size = new System.Drawing.Size(74, 18);
             this.EmpId.TabIndex = 26;
             this.EmpId.Text = "Emp ID ";
+            this.EmpId.Click += new System.EventHandler(this.EmpId_Click);
             // 
             // mainHeading
             // 
             this.mainHeading.AutoSize = true;
             this.mainHeading.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.mainHeading.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mainHeading.Location = new System.Drawing.Point(486, 38);
+            this.mainHeading.Location = new System.Drawing.Point(432, 30);
             this.mainHeading.Name = "mainHeading";
-            this.mainHeading.Size = new System.Drawing.Size(416, 36);
+            this.mainHeading.Size = new System.Drawing.Size(359, 31);
             this.mainHeading.TabIndex = 25;
             this.mainHeading.Text = "Update Employee In DataBase";
             // 
@@ -162,9 +169,10 @@ namespace DesktopProject
             this.backToHome.Cursor = System.Windows.Forms.Cursors.Hand;
             this.backToHome.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.backToHome.ForeColor = System.Drawing.SystemColors.Info;
-            this.backToHome.Location = new System.Drawing.Point(27, 482);
+            this.backToHome.Location = new System.Drawing.Point(24, 386);
+            this.backToHome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.backToHome.Name = "backToHome";
-            this.backToHome.Size = new System.Drawing.Size(249, 50);
+            this.backToHome.Size = new System.Drawing.Size(221, 40);
             this.backToHome.TabIndex = 37;
             this.backToHome.Text = "BACK TO HOME";
             this.backToHome.UseMnemonic = false;
@@ -174,17 +182,17 @@ namespace DesktopProject
             // footer
             // 
             this.footer.AutoSize = true;
-            this.footer.Location = new System.Drawing.Point(1092, 659);
+            this.footer.Location = new System.Drawing.Point(971, 527);
             this.footer.Name = "footer";
-            this.footer.Size = new System.Drawing.Size(177, 20);
+            this.footer.Size = new System.Drawing.Size(151, 16);
             this.footer.TabIndex = 38;
             this.footer.Text = "Project By Abhinav Arya";
             // 
             // UpdateEmployee
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1325, 717);
+            this.ClientSize = new System.Drawing.Size(1178, 574);
             this.Controls.Add(this.footer);
             this.Controls.Add(this.backToHome);
             this.Controls.Add(this.UpdateEmpButton);
@@ -199,6 +207,7 @@ namespace DesktopProject
             this.Controls.Add(this.addEmpIdTxtBox);
             this.Controls.Add(this.EmpId);
             this.Controls.Add(this.mainHeading);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "UpdateEmployee";
             this.Text = "UpdateEmployee";
             this.ResumeLayout(false);
